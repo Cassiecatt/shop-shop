fd739aae0d8e8e145927908fde6ed833b09918db
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-22 17:57:19 -0700

    added offline functionality

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
   const [, dispatch] = useStoreContext();
@@ -10,22 +11,26 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id,
     });
+    idbPromise("cart", "delete", { ...item });
   };
 
   const onChange = (e) => {
     const value = e.target.value;
-  
-    if (value === '0') {
+
+    if (value === "0") {
       dispatch({
         type: REMOVE_FROM_CART,
-        _id: item._id
+        _id: item._id,
       });
+
+      idbPromise('cart', 'delete', { ...item });
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
-        purchaseQuantity: parseInt(value)
+        purchaseQuantity: parseInt(value),
       });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
     }
   };
 
@@ -40,8 +45,17 @@ const CartItem = ({ item }) => {
         </div>
         <div>
           <span>Qty:</span>
-          <input type="number" placeholder="1" value={item.purchaseQuantity} onChange={onChange} />
-          <span role="img" aria-label="trash" onClick={() => removeFromCart(item)}>
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
             🗑️
           </span>
         </div>
